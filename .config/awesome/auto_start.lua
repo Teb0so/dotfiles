517819ee3a7226101e3a1b5de17dc324f1a1f268
Teb0so <joaopedrostcunha@gmail.com>
2025-08-27 19:43:08 -0300

    improved some awesome configs

--- .config/awesome/auto_start.lua
+++ .config/awesome/auto_start.lua
@@ -3,6 +3,8 @@ local awful = require("awful")
 awful.spawn.with_shell("xinput --set-prop 'pointer:''SINOWEALTH Wired Gaming Mouse' 'libinput Accel Profile Enabled' 0, 1")
 awful.spawn.with_shell("xinput --set-prop 'pointer:''SINOWEALTH Wired Gaming Mouse' 'libinput Accel Speed' -0.4")
 
+awful.spawn.with_shell("$HOME/.screenlayout/TWO.sh")
+
 awful.spawn.with_shell("waypaper --restore")
 
 awful.spawn.once("xset s off -dpms")
