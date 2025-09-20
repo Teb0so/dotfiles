f90f50de6af7082335573b9529abe1aa8deb269e
Teb0so <joaopedrostcunha@gmail.com>
2025-09-20 00:17:28 -0300

    added polkit config

--- .config/awesome/auto_start.lua
+++ .config/awesome/auto_start.lua
@@ -1,14 +1,9 @@
 local awful = require("awful")
 
-awful.spawn.with_shell([[
-  killall xdg-desktop-portal xdg-desktop-portal-kde;
-  sleep 1;
-  /usr/lib/x86_64-linux-gnu/libexec/xdg-desktop-portal-kde &
-  /usr/libexec/xdg-desktop-portal &
-]])
-
 awful.spawn.with_shell("xinput --set-prop 'pointer:''SINOWEALTH Wired Gaming Mouse' 'libinput Accel Profile Enabled' 0, 1")
 awful.spawn.with_shell("xinput --set-prop 'pointer:''SINOWEALTH Wired Gaming Mouse' 'libinput Accel Speed' -0.4")
+
+awful.spawn.with_shell("lxqt-policykit-agent")
 
 awful.spawn.with_shell("$HOME/.screenlayout/TWO.sh")
 
