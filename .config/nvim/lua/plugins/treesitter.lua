ffa678ff8f4acf746f20c7dfd0d7720ae17a9820
Teb0so <joaopedrostcunha@gmail.com>
2025-11-05 15:37:52 -0300

    minor changes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,19 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup {
+            -- lua is installed to stop errors from appearing every time I open a lua files
+            -- markdown is installed for the markdown renderer
             ensure_installed = { 'lua', 'markdown' },
             highlight = { enable = false },
+
+            -- Disable highlight for lua files
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "lua",
+                callback = function()
+                    vim.treesitter.stop(0)
+                end,
+})
+
         }
     end,
 }
