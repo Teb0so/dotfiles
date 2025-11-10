910ff9845a2d623c30e432daacd8ce6a1c9e86da
Teb0so <joaopedrostcunha@gmail.com>
2025-11-09 21:58:57 -0300

    added spellcheck for nvim

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -2,4 +2,12 @@ return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     opts = {},
+    config = function ()
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "md", "markdown" },
+            callback = function()
+                vim.cmd("setlocal spell spelllang=pt,en")
+            end,
+        })
+    end
 }
