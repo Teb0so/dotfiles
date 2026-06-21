0e13b46110c6e9510c3d7cd82a6aa4ab1f24e32d
Teb0so <joaopedrostcunha@gmail.com>
2026-06-21 03:26:10 -0300

    remove line numbers in md and txt files

--- .config/nvim/lua/configs/markdown.lua
+++ .config/nvim/lua/configs/markdown.lua
@@ -1,6 +1,8 @@
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "markdown", "text" },
     callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
         vim.opt_local.linebreak=true
         vim.opt_local.list=false
         vim.opt_local.conceallevel=2
